5ea0f6f2386cbb2c50f663075df0d3c2d66e00ea
wacky <wacky0122@163.com>
2018-11-27 18:35:49 +0800

    shell调用时，通过echo输出内容来获取返回值及exit code

--- Main.php
+++ Main.php
@@ -8,28 +8,29 @@
 spl_autoload_register(function ($name) {
     $file = __DIR__ .DIRECTORY_SEPARATOR.$name.".php";
     $file = str_replace("\\", DIRECTORY_SEPARATOR, $file);
-    echo "Want to load $name->$file.\n";
+    //echo "Want to load $name->$file.\n";
     if (file_exists($file)) {
         include_once $file;
     }
 });
 
-var_dump(count($argv));
+//var_dump(count($argv));
 if (count($argv) < 2) {
-    echo "缺少文件路径参数！";
+    //echo "缺少文件路径参数！";
     //$converter = new Html2Markdown();
     //var_dump ($converter->convertCalibre("C:/Users/Administrator/Desktop/test/ttt.zip"));
-    exit;
+    exit(1);
 }
 $sourceFile = $argv[1];
 if (file_exists($sourceFile)) {
     $converter = new Html2Markdown();
     $targetFile = $converter->convertCalibre($sourceFile);
-    var_dump ($targetFile);
+    #var_dump ($targetFile);
+    echo $targetFile;
 } else {
     echo "文件不存在：".$sourceFile;
+    exit(1);
 }
-
 //php Main.php C:\Users\Administrator\Desktop\test\ttt.zip
 //var_dump ($converter->convertCalibre("C:/Users/Administrator/Desktop/test/ttt.zip"));
 //var_dump ($converter->convertCalibre("C:/Users/Administrator/Desktop/test/part0017.html"));
